7e756e9963b6de26417751910532eddab9289956
sumit jadhav <sumitjadhav3710@gmail.com>
2023-09-26 14:08:55 +0530

    grid pages

--- education-generic/src/app/memberform/memberform.component.ts
+++ education-generic/src/app/memberform/memberform.component.ts
@@ -6,10 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./memberform.component.css']
 })
 export class MemberformComponent {
-  tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
+  memberform: boolean = false;
+  closeformflag: boolean = false;
+  
+
+  onAction() {
+    this.memberform = true;
+    console.log("Done");
+  }
+  closeDialog() {
+    this.memberform = false;
+  }
 }
